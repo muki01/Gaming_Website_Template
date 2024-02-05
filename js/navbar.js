738360f4ed0f39f52d7f360ae56b34c8edfd592d
Muksin Muksin <75759731+muki01@users.noreply.github.com>
2024-02-05 14:10:55 +0200

    Animated Menu Button

--- js/navbar.js
+++ js/navbar.js
@@ -1,9 +1,9 @@
 const menuBtn = document.querySelector(".menuBtn");
+const navBar = document.querySelector(".navBar");
 menuBtn.addEventListener("click", navToggle);
 
 function navToggle() {
-  menuBtn.classList.toggle("active");
-  const navBar = document.querySelector(".navBar");
+  menuBtn.classList.toggle("openmenu");
   navBar.classList.toggle("open");
   if (navBar.classList.contains("open")) {
     navBar.style.maxHeight = navBar.scrollHeight + "px";
